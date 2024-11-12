f6bea9d44c8fe873ab6eceb59209e90ff5c83fd1
Daffadon <daffaputranarendra9@gmail.com>
2024-11-12 09:34:20 +0700

    refactor(component): change input with component

--- src/components/input/input_with_icon.tsx
+++ src/components/input/input_with_icon.tsx
@@ -1,20 +1,29 @@
 import { FormControl, Input, InputAdornment, InputLabel } from "@mui/material"
 import React, { ReactElement } from "react";
+import { ActionType } from "../../data/dto/action_type";
 
 interface InputIconProps {
   icon: ReactElement
   label: string;
   type: inputType
+  value: string | undefined
+  dispatch: React.Dispatch<ActionType>
+  actionType: string
+  required?: boolean
 }
-type inputType = "email" | "password" |"text"
 
-const InputIcon = ({ icon, label, type }: InputIconProps) => {
+type inputType = "email" | "password" | "text"
+
+const InputIcon = ({ icon, label, type, value, dispatch, actionType, required }: InputIconProps) => {
   return (
-    <FormControl variant="standard" sx={{ width: "100%" }}>
+    <FormControl variant="standard" sx={{ width: "100%" }} required={required}>
       <InputLabel sx={{ color: "white" }} >
         {label}
       </InputLabel>
       <Input
+        value={value}
+        onChange={(e) => dispatch({ type: actionType, payload: e.target.value })}
+        placeholder={label}
         type={type}
         startAdornment={
           <InputAdornment position="start">
