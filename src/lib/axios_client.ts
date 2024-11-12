9ceb32e4be65df3cb4caf95e7e45d53f061397ce
Daffadon <daffaputranarendra9@gmail.com>
2024-11-12 09:37:06 +0700

    feat(instance): add axios instance custom implementation

---
+++ src/lib/axios_client.ts
@@ -0,0 +1,33 @@
+import axios from "axios";
+import Cookies from "js-cookie";
+
+export const axiosClient = axios.create({
+  baseURL: `${import.meta.env.VITE_APP_BASE_URL}/`,
+});
+
+axiosClient.interceptors.request.use((config) => {
+  // const token = localStorage.getItem("ACCESS_TOKEN");
+  const token = Cookies.get("ACCESS_TOKEN");
+  config.headers.Authorization = `Bearer ${token}`;
+  return config;
+});
+
+axiosClient.interceptors.response.use(
+  (response) => {
+    return response;
+  },
+  (error) => {
+    const { response } = error;
+    if (response.status === 401) {
+      // localStorage.removeItem("ACCESS_TOKEN");
+      Cookies.remove("ACCESS_TOKEN");
+      window.location.href = "/";
+    }
+    if (response.status === 403 && response.data.message === "Invalid token") {
+      // localStorage.removeItem("ACCESS_TOKEN");
+      Cookies.remove("ACCESS_TOKEN");
+      window.location.href = "/";
+    }
+    throw error;
+  }
+);
